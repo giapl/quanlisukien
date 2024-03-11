9c3eae10bfde12f35bfb6e6089e4b87856509185
giapl <ledanggiap1704@gmail.com>
2024-03-11 14:53:36 +0700

    final code

--- src/main/java/org/example/quanlisukien/controller/feedbacks/FeedbacksController.java
+++ src/main/java/org/example/quanlisukien/controller/feedbacks/FeedbacksController.java
@@ -1,6 +1,5 @@
 package org.example.quanlisukien.controller.feedbacks;
 
-import jakarta.validation.Valid;
 import org.example.quanlisukien.data.request.FeedbackRequest;
 import org.example.quanlisukien.service.feedbacks.FeedbackService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,7 +25,7 @@ public class FeedbacksController {
   }
 
   @PostMapping("/create")
-  public ResponseEntity<?> createFeedback(@Valid @RequestBody FeedbackRequest feedbackRequest) {
+  public ResponseEntity<?> createFeedback(@RequestBody FeedbackRequest feedbackRequest) {
     return ResponseEntity.ok(feedbackService.createFeedback(feedbackRequest));
   }
 
--- src/main/java/org/example/quanlisukien/data/request/FeedbackRequest.java
+++ src/main/java/org/example/quanlisukien/data/request/FeedbackRequest.java
@@ -1,6 +1,5 @@
 package org.example.quanlisukien.data.request;
 
-import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,7 +13,6 @@ public class FeedbackRequest {
 
   private Long event_id;
 
-  @NotEmpty(message = "không được để trống bình luận")
   private String feedback_content;
 
   private String feedback_image;
